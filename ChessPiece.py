640a33bc3b77f0e3435e37e65ed120c0f19d88ff
spirob <47575005+spirob@users.noreply.github.com>
2019-10-27 15:38:19 -0400

    Updated Chess Piece class, Queen class, and new Pawn class
    The check_move method was moved from the queen class to the Chess Piece class due as its implementation works for the majority of chess pieces. A Pawn class was created with the valid_move method ensure pawn pieces follow the right movement pattern
    
    The pawn class is essential for the game as without it pawns would not move properly. Now pawns move as expected.

--- ChessPiece.py
+++ ChessPiece.py
@@ -14,3 +14,49 @@ class ChessPiece(ABC):
 
     def return_position(self):
         return self.position
+
+
+    '''
+    checks to see if there are any pieces between starting position and position
+    the piece is being moved to. Position is the starting position of the piece,
+    (row,col) is the desired position, and board is the board being used. 
+
+    '''
+    def checkMove(self, position, row, col, board):
+
+        if not validCoordinates(position, row, col):
+            return False
+        
+        r = False
+        l = False
+        f = False
+        b = False
+        
+        check_position=[position[0], position[1]]
+        if col>position[1]: # Piece moves right
+            check_position[1] += 1
+            r = True
+
+        elif col<position[1]: # Piece moves left
+            check_position[1] += - 1
+            l = True
+
+        elif row>position[0]: # Pice moves forward
+            check_position[0] += 1
+            f = True
+
+        elif row<position[0]: # Piece moves backwards
+            check_position[0] += -1
+            b = True
+
+        while board[check_position[0]][check_position[1]] == "E" and check_position[0]<row and check_position[1]<col:  # check whether every square in between is empty
+            if l == True:
+                check_position[1] += - 1
+            if r == True:
+                check_position[1] += 1
+            if f == True:
+                check_position[0] += 1
+            if b == True:
+                check_position[0] += -1
+                                        :           
+        return board[check_position[0]][check_position[1]] == 'E'
--- Pawn.py
+++ Pawn.py
@@ -2,12 +2,10 @@ from array import *
 from ChessPiece import ChessPiece
 
 
-class Queen(ChessPiece):
+class Pawn(ChessPiece):
 
-    """Position is the current position of Queen. Row and col is where the piece should be moved.
-       The board is a two dimensional array which contains the players and their corresponding piece colour.
-       eg. at board[1,1] there might be a white piece represented by "W".
-       Black piece represented by "B" and empty represented by "E"
+    """
+    Changes methods to fit that of a pawn. ie valid_coordinates are only up one diagonally on the board. 
     """
 
     def __init__(self, colour, position):
@@ -15,18 +13,19 @@ class Queen(ChessPiece):
 
 
 
-    """Position is the current position of Queen. Row and col is where the piece should be moved.
+      """Position is the current position of Pawn. Row and col is where the piece should be moved.
        The board is a two dimensional array which contains the players and their corresponding piece colour.
        eg. at board[1,1] there might be a white piece represented by "W".
-       Black piece represented by "B" and empty represented by "E". Ensures piece is moving to a valid location on
-       on the board following its movement capabilities.
+       Black piece represented by "B" and empty represented by "E"
     """
-    
     def valid_coordinates (self, position, row, col):
 
         if row<0 or row>7:   #Checks to ensure coordinates are on board
             return False
         if col<0 or col>7:
+            return False
+
+        if position[0]<=row: #ensures piece is moving forward
             return False
         
         rise = row-position[0]
@@ -34,24 +33,9 @@ class Queen(ChessPiece):
 
         if rise == 0 and run ==0: #checks to make sure piece isn't moving to current location 
             return False
-        elif rise == 0 or run == 0: #checks to see if piece is moving up/down or side/side
-            return True
         elif rise/run == -1 or rise/run ==1: #checks to see if piece is moving diagonally
             return True
         else:
             return False
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        
