2fab5fc28c81b65514cdc59395b285da765cf7c7
Alees Ahmad Goraya <alees.goraya@mail.utoronto.ca>
2019-10-27 17:32:56 -0400

    Comments and docstrings added

--- Rook.py
+++ Rook.py
@@ -1,44 +1,58 @@
 from array import *
+
 from ChessPiece import ChessPiece
 
 
 class Rook(ChessPiece):
-
-    """position is the current position of rook. row and col given for where the piece should be moved.
-       board is a two dimensional array which contains player has what pieces on the board.
-       for example at board[1,1] there might be a white piece represented by "W".
-       Black piece represented by "B" and empty represented by "E"
     """
-    def check_move(self, position, row, col, board):
-        check_position = [0,0]
+       position is the current position of rook. row and col given for where
+       the piece should be moved.
+       board is a two dimensional array containing which players
+       have what pieces on the board.
+       for example
+       at [1,1] there might be a white piece represented by "W".
+       At [2,1] there might be a Black piece represented by "B"
+       At [3,1] empty represented by "E"
+    """
 
-        if col > position[1]: # Rook moves towards right
-            check_position[1] = position[1]+1
-            check_position[0]=position[0]
+    def check_move(self, position, row, col, board):
+
+        check_position = [0, 0]
+
+        # Rook moves right Column changes(increases) row doesnt
+        if col > position[1]:
+
+            check_position[1] = position[1] + 1
+            check_position[0] = position[0]
             move = "r"
 
-        elif col<position[1]: # Rook moves left
+        # Rook moves left Column changes(decreases) row doesnt
+        elif col < position[1]:
+
             check_position[1] = position[1] - 1
             check_position[0] = position[0]
             move = "l"
 
-        elif row>position[0]: # Rook moves forward
+        # Rook moves forward row changes(increases) Column doesnt
+        elif row > position[0]:
+
             check_position[0] = position[0] + 1
             check_position[1] = position[1]
             move = "f"
 
-        elif row<position[0]: # Rook moves backwards
-            check_position[0] = position[0]-1
+        # Rook moves backwards row changes(decreases) Column doesnt
+        elif row < position[0]:
+
+            check_position[0] = position[0] - 1
             check_position[1] = position[1]
             move = "b"
 
-        print(check_position[0])
-        print(check_position[1])
+        """check whether every square is empty between rook and point to move. 
+           Keep checking until position is reached 
+           or a piece found"""
+        while board[check_position[0]][check_position[1]] == "E" and (
+                row != check_position[0] or col != check_position[1]):
 
-        """check whether every square is empty between rook and point to move. Keep checking until position is reached 
-           or a piece found
-        """
-        while board[check_position[0]][check_position[1]] == "E" and (row != check_position[0] or col != check_position[1]):
             if move == "b":
                 check_position[0] = check_position[0] - 1
             elif move == "f":
@@ -47,21 +61,27 @@ class Rook(ChessPiece):
                 check_position[1] = check_position[1] - 1
             elif move == "r":
                 check_position[1] = check_position[1] + 1
-        # First check to see if a piece was found in way before reaching position if yes then return false
-        # as move cannot be made
+
+        # First check to see if a piece was found in way before reaching
+        # position if yes then return false as move cannot be made
         if check_position[0] == row and check_position[1] == col:
-            # check if there is already a player piece there then move cannot be made
+
+            # check if there is already a player piece there then move
+            # cannot be made
             if board[row][col] == self.color:
+
                 return False
 
-            # check if the piece found is for other player. Player can take the piece and assume its position
+            # check if the piece found is for other player
+            # Player can take the piece and assume its position
             if board[row][col] == "B":
 
                 return True
 
-            return True     # This means that the row and col given was empty so piece can be placed
-
+            return True  # This means that the row and col given was empty
+                          # so piece can be placed
         else:
+            
             return False
 
 
@@ -82,23 +102,13 @@ Board will be represented the following way in 2-d array
 Testing out the class
 """
 p1 = Rook("B", [5, 6])
-board = [["B","B","B","B","B","B","B","B"],
-         ["B","B","B","B","B","B","B","B"],
-         ["E","E","E","E","E","E","E","E"],
-         ["E","E","E","E","E","E","E","E"],
-         ["E","E","E","E","E","E","E","E"],
-         ["E","E","E","E","E","E","E","E"],
-         ["W","W","W","W","W","W","W","W"],
-         ["W","W","W","W","W","W","W","W"]
+board = [["B", "B", "B", "B", "B", "B", "B", "B"],
+         ["B", "B", "B", "B", "B", "B", "B", "B"],
+         ["E", "E", "E", "E", "E", "E", "E", "E"],
+         ["E", "E", "E", "E", "E", "E", "E", "E"],
+         ["E", "E", "E", "E", "E", "E", "E", "E"],
+         ["E", "E", "E", "E", "E", "E", "E", "E"],
+         ["W", "W", "W", "W", "W", "W", "W", "W"],
+         ["W", "W", "W", "W", "W", "W", "W", "W"]
          ]
 print(p1.check_move(p1.position, 1, 6, board))
-
-
-
-
-
-
-
-
-
-
